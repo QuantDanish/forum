401b91aee83b534ea4fa897e6f6596ee6def5265
QuantDanish <35726243+QuantDanish@users.noreply.github.com>
2018-07-21 17:46:47 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -4,8 +4,8 @@
 
 module.exports = {
     google: {
-        clientID: '173727189988-bgb3p598ajhbh6tbd69lglm28gfv6nq2.apps.googleusercontent.com',
-        clientSecret: 'Ng0K60xJ1tW9TaFqYmo_itwP'
+        clientID: '173727189988-bgb3p598ajhbh6tbv6nq2.apps.googleusercontent.com',
+        clientSecret: 'Ng0K60xJ1tWmo_itwP'
     },
     mongodb: {
         url: 'mongodb://localhost:27017/forum'
